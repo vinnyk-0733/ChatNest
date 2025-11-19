c3021692a49af0b506db89e981190eb79b08fa42
vinayak_0733 <vinayakumar464@gmail.com>
2025-11-19 10:25:57 +0530

    changes done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import { generateToken } from "../lib/utils.js"
-import User from "../models/user.model.js "
+import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import cloudinary  from "../lib/cloudinary.js"
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -12,26 +12,18 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// Fetch all chat users except logged-in user
 router.get("/users", protectRoute, getUsersForSidebar);
 
-// Get all messages between current user and selected user
 router.get("/:id", protectRoute, getMessages);
 
-// Send a message (text/image)
 router.post("/send/:id", protectRoute, sendMessage);
 
-// Edit a message
 router.put("/:id", protectRoute, editMessage);
 
-// Delete message (for self / for both)
 router.delete("/:id", protectRoute, deleteMessage);
 
-// Search messages between logged-in user and another user
 router.get("/search/:id", protectRoute, searchMessages);
 
-// ✅ React to a message (toggle/change reaction) - new route
-// Body: { emoji: "👍" }
 router.post("/:id/react", protectRoute, reactToMessage);
 
 export default router;
